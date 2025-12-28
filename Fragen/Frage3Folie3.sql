b786cf140ee434f0fb6b6b90f597569e15cf1b8e
muma1457 <muma1457@bht-berlin.de>
2025-12-28 12:38:15 +0100

    Frage3Folie3fertig

--- Fragen/Frage3Folie3.sql
+++ Fragen/Frage3Folie3.sql
@@ -1,6 +1,24 @@
 -- Welche Orte wurden mit Formel-1-Rennen in Verbindung gesucht?
 
-select query 
-from f1_filtered f
-where query
-ilike 
+SELECT query
+FROM f1_filtered f
+WHERE
+(	   query ILIKE '%Bahrain%'
+    OR query ILIKE '%Malaysia%'
+    OR query ILIKE '%Australia%'
+    OR query ILIKE '%Imola%'
+    OR query ILIKE '%Nurburg%'
+    OR query ILIKE '%Spain%'
+    OR query ILIKE '%Monaco%'
+)
+AND (
+    query ILIKE '%grand prix%'
+    OR		   query ILIKE '%race%'
+        OR query ILIKE '%Bahrain%'
+        OR query ILIKE '%Malaysia%'
+        OR query ILIKE '%Australia%'
+        OR query ILIKE '%Imola%'
+        OR query ILIKE '%Nurburg%'
+        OR query ILIKE '%Spain%'
+        OR query ILIKE '%Monaco%'
+    );
