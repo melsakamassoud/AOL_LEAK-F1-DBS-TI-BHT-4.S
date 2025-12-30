f8f4a56479133b7cf394f36b6518fb6638f16aa1
DVU <72096733+DamVeUrb@users.noreply.github.com>
2025-12-30 14:00:51 +0100

    Besser für Pichart

--- Fragen/Folie10.sql
+++ Fragen/Folie10.sql
@@ -1,17 +1,7 @@
--- Wie viele Nutzer sind im Datensatz in Relation zu F1 
--- mehr als einmal über die 3 Monate zu finden?
+-- Wie viele Nutzer sind im Datensatz in Relation zu F1 mehr als einmal über die 3 Monate zu finden?
 
-SELECT 
-    COUNT(*) FILTER (WHERE query_count > 1) AS wiederkehrende_usr,
-    COUNT(*) AS gesamt_usr,
-    ROUND(
-        (COUNT(*) FILTER (WHERE query_count > 1)::numeric / COUNT(*)) * 100,
-        2 -- ROUND ()2 
-    ) || '%' AS verhaeltnis_proz
-FROM (
-    SELECT anon_id, COUNT(*) AS query_count 
-    FROM F1_Query
-    GROUP BY anon_id
-) AS user_stats;
-
-
+SELECT 'Wiederkehrend' AS status, COUNT(*) AS anzahl
+FROM (SELECT anon_id FROM F1_Query GROUP BY anon_id HAVING COUNT(*) > 1) AS sub
+UNION ALL
+SELECT 'Einmalig' AS status, COUNT(*) AS anzahl
+FROM (SELECT anon_id FROM F1_Query GROUP BY anon_id HAVING COUNT(*) = 1) AS sub;
