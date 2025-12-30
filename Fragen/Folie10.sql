6b856eb62fb4fab933b65d166e09fecd6c0ff8bd
DVU <72096733+DamVeUrb@users.noreply.github.com>
2025-12-30 14:02:39 +0100

    Add count for users appearing twice in dataset

--- Fragen/Folie10.sql
+++ Fragen/Folie10.sql
@@ -4,4 +4,7 @@ SELECT 'Wiederkehrend' AS status, COUNT(*) AS anzahl
 FROM (SELECT anon_id FROM F1_Query GROUP BY anon_id HAVING COUNT(*) > 1) AS sub
 UNION ALL
 SELECT 'Einmalig' AS status, COUNT(*) AS anzahl
-FROM (SELECT anon_id FROM F1_Query GROUP BY anon_id HAVING COUNT(*) = 1) AS sub;
+FROM (SELECT anon_id FROM F1_Query GROUP BY anon_id HAVING COUNT(*) = 1) AS sub
+UNION ALL
+SELECT 'Zweimal' AS status, COUNT(*) AS anzahl
+FROM (SELECT anon_id FROM F1_Query GROUP BY anon_id HAVING COUNT(*) = 2) AS sub;
